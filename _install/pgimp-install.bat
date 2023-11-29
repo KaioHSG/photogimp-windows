d19f954f7e6924930b48cf88d66c4b3e15436f9e
Kaio HSG <96930584+kaiohsg@users.noreply.github.com>
2023-11-29 20:43:20 -0300

    Update pgimp-install.bat

--- _install/pgimp-install.bat
+++ _install/pgimp-install.bat
@@ -1,9 +1,7 @@
 @echo off
 title Install PhotoGIMP Windows
-if not exist "%UserProfile%\AppData\Roaming\GIMP\2.10" (
-    mkdir "%UserProfile%\AppData\Roaming\GIMP\2.10"
-)
-if not exist "PhotoGIMP.zi0p" (
+if not exist "%UserProfile%\AppData\Roaming\GIMP\2.10" mkdir "%UserProfile%\AppData\Roaming\GIMP\2.10"
+if not exist "PhotoGIMP.zip" (
     echo You need the "PhotoGIMP.zip". Make sure the file is in the same directory.
     echo Download here: https://github.com/Diolinux/PhotoGIMP/releases/latest
     pause > nul
@@ -14,6 +12,6 @@ if %ErrorLevel% equ 2 (exit)
 echo -------------------------------------------------
 tar --strip=0 -xvzf "PhotoGIMP.zip" -C "%UserProfile%\AppData\Roaming\GIMP\2.10" "PhotoGIMP-master/.var/app/org.gimp.GIMP/config/GIMP/2.10"
 echo =================================================
-echo Press any key to exit...
+echo Finish.
 pause > nul
 exit
