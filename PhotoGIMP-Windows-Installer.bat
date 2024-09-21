30120774e304d1c0ee3f4ff97a9a9f3d58df55e2
Kaio HSG <kaio12hsg@gmail.com>
2024-09-21 14:01:31 -0300

    Update 1.4

---
+++ PhotoGIMP-Windows-Installer.bat
@@ -0,0 +1,30 @@
+@echo off
+set version=1.4
+title Installer PhotoGIMP Windows v%version%
+echo PhotoGIMP Windows Installer v%version%
+echo ==================================================
+choice /c yn /m "This will erase your settings (%AppData%\GIMP). Continue"
+if %ErrorLevel% equ 2 (exit)
+ping /n 1 github.com > nul
+if %errorLevel% neq 0 (
+    if not exist "photogimp-data.zip" (
+        echo ##################################################
+        echo No server connection. Please check your internet connection and try again.
+        pause > nul
+        exit
+    )
+    goto :install
+)
+curl -s https://api.github.com/repos/Diolinux/PhotoGIMP/releases/latest > %temp%\latest-release.json
+for /f "tokens=3 delims=:" %%A in ('findstr /i "browser_download_url" %temp%\latest-release.json') do set url=https:%%A
+del /q "%temp%\latest-release.json"
+echo --------------------------------------------------
+curl -L -o "photogimp-data.zip" %url%
+
+:install
+echo -------------------------------------------------
+tar -zxvf "photogimp-data.zip" -C "%AppData%" --strip-components=5 "PhotoGIMP-master/.var/app/org.gimp.GIMP/config/GIMP"
+echo ##################################################
+echo Finish.
+pause > nul
+exit
