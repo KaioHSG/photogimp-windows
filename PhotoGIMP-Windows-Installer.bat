6b90ae3c350f5473ca2be13a0a028d9c2edad94f
Kaio HSG <kaio12hsg@gmail.com>
2024-09-21 17:49:37 -0300

    Update 1.4

--- PhotoGIMP-Windows-Installer.bat
+++ PhotoGIMP-Windows-Installer.bat
@@ -2,12 +2,14 @@
 set version=1.4
 title PhotoGIMP Windows Installer v%version%
 echo PhotoGIMP Windows Installer v%version%
-echo ==================================================
-choice /c yn /m "This will erase your settings (%AppData%\GIMP). Continue"
-if %ErrorLevel% equ 2 (exit)
+if exist "%AppData%\GIMP" (
+    echo --------------------------------------------------
+    choice /c yn /m "This will erase your settings (%AppData%\GIMP). Continue"
+    if %ErrorLevel% equ 2 (exit)
+)
 ping /n 1 github.com > nul
 if %errorLevel% neq 0 (
-    if not exist "photogimp-data.zip" (
+    if not exist "photogimp.zip  " (
         echo ##################################################
         echo No server connection. Please check your internet connection and try again.
         pause > nul
@@ -18,15 +20,18 @@ if %errorLevel% neq 0 (
 curl -s https://api.github.com/repos/Diolinux/PhotoGIMP/releases/latest > %temp%\latest-release.json
 for /f "tokens=3 delims=:" %%A in ('findstr /i "browser_download_url" %temp%\latest-release.json') do set url=https:%%A
 del /q "%temp%\latest-release.json"
-if exist "photogimp-data.zip" (
-    del /q "photogimp-data.zip"
+if exist "photogimp.zip  " (
+    del /q "photogimp.zip  "
 )
 echo --------------------------------------------------
-curl -L -o "photogimp-data.zip" %url%
+curl -L -o "photogimp.zip  " %url%
 
 :install
+if not exist "%AppData%\GIMP" (
+    mkdir "%AppData%\GIMP"
+)
 echo -------------------------------------------------
-tar -zxvf "photogimp-data.zip" -C "%AppData%/GIMP" --strip-components=6 "PhotoGIMP-master/.var/app/org.gimp.GIMP/config/GIMP/*"
+tar -zxvf "photogimp.zip  " -C "%AppData%/GIMP" --strip-components=6 "PhotoGIMP-master/.var/app/org.gimp.GIMP/config/GIMP/*"
 echo ##################################################
 echo Install finish.
 pause > nul
