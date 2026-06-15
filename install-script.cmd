388148c6a051450f0ca17f38bc50227a7a8444c2
Kaio HSG <kaio12hsg@gmail.com>
2026-06-15 15:54:20 -0300

    Update install-script.cmd

--- install-script.cmd
+++ install-script.cmd
@@ -1,19 +1,29 @@
 @echo off
+
 set "gimpVersion=3.2"
 set "version=1.7"
+
 title PhotoGIMP Windows Installer (v%version%)
+
 echo PhotoGIMP Windows for GIMP %gimpVersion%
 echo Download GIMP in: https://download.gimp.org/gimp/v%gimpVersion%/windows/
 echo --------------------------------------------------
-if not exist "%appData%\GIMP\%gimpVersion%" (
-    choice /c yn /m "Start installation"
-    if %errorLevel% equ 2 (exit /b)
-) else (
-    choice /c yn /m "This will erase your config (%appData%\GIMP\%gimpVersion%). Continue"
-    if %errorLevel% equ 2 (exit /b)
-    echo -------------------------------------------------
-    xcopy "%appData%\GIMP\%gimpVersion%\" "%appData%\GIMP\%gimpVersion%-bak\" /s /-y
-)
+if exist "%appData%\GIMP\%gimpVersion%" (goto :nexist) else (goto :exist)
+
+:exist
+choice /c yn /m "Start installation"
+if %errorLevel% equ 2 exit /b
+goto :install
+
+:noexist
+choice /c yn /m "This will erase your config (%appData%\GIMP\%gimpVersion%). Continue"
+if %errorLevel% equ 2 exit /b
+echo %errorlevel%
+echo -------------------------------------------------
+xcopy "%appData%\GIMP\%gimpVersion%\" "%appData%\GIMP\%gimpVersion%-bak\" /s /-y
+goto :install
+
+:install
 echo -------------------------------------------------
 xcopy ".\%gimpVersion%\" "%appData%\GIMP\%gimpVersion%\" /s /y
 echo ##################################################
